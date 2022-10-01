6f672952b310f7004ed8815236aa3ccf7b164d6c
SergioOjeda <114820931+SergioOjeda@users.noreply.github.com>
2022-10-01 11:07:59 -0500

    Revert "Se quitan instrucciones comentadas"
    This reverts commit fb657ebd4be0613a9ac8bc737ace485632d78e23.

--- rp2_sim.py
+++ rp2_sim.py
@@ -1,16 +1,24 @@
 def asm_pio(*args, **kwargs):
     def decorador(programa):
+        print("Inside decorator")
         def compilador():
-            print("Parámetros", kwargs) 
+            print("Inside inner function")
+            print("I like", kwargs) 
             programa()
             return None
         return compilador
     return decorador
 
+
+
+#lista_instr=[]
 def decorador_instr(fun_inst):
+    # global 
     def decoracion_instr(self,*args, **kwargs):
-        fun_inst(self,*args, **kwargs)
-        return None 
+        #global lista_instr
+        instr1=fun_inst(self,*args, **kwargs)
+        #lista_instr.append(instr1)
+        return None #instr1
     return decoracion_instr
 
 pins='pins'
@@ -21,38 +29,59 @@ class PIO():
 
 class StateMachine:
   def __init__(self, id_, program, freq=125000000, **kwargs):
-        global sm_iniciandose,fsms
-        sm_iniciandose=self
-        #print('StateMachine.__init__',id_, program, freq, kwargs)
+        global sm_iniciandoce,fsms
+        sm_iniciandoce=self
+        print('StateMachine.__init__',id_, program, freq, kwargs)
         self.lista_instr=[]
         program()
-        print('Fueron leidas',len(self.lista_instr), 'instrucciones')
-        sm_iniciandose=None
+        print('lista_instr',self.lista_instr)
+        sm_iniciandoce=None
         fsms[id_]=self
         pass
       
+#   def add_instr(self):
+#      self.lista_instr
         
   def active(self, x=None):
-    '''Esta rutina simula exclisivamnte esa FSM. Sería interesante crear simulación en parlelo con otras FSM'''
-    if x==1:
-        print('Está pendiente de realizar la simulacón')
-
+       pass
 fsms=[None]*8
 
-sm_iniciandose=None    
+sm_iniciandoce=None
+
+# class Instruc():
+#     def __init__(self):
+#         pass
+
+
+
+# @decorador_instr
+# def nop():
+#     return'nop'
+    
+# @decorador_instr
+# def set():
+#     return'set'
+    
+# #@decorador_instr
+# def wrap_target():
+#     return'wrap_target'
+    
+# @decorador_instr
+# def wrap():
+#     return'wrap'
+    
 
 
 class nop:
     @decorador_instr
     def __init__(self,*args, **kwargs):
-        global sm_iniciandose
-        print(self.__class__.__name__)#,'nop.__init__',args,kwargs)
-        sm_iniciandose.lista_instr.append(self)
-
+        global sm_iniciandoce
+        print(self.__class__.__name__,'nop.__init__',args,kwargs)
+        sm_iniciandoce.lista_instr.append(self)
         pass
      
     def __getitem__(self,name):
-        #print('nop.__getattr__',name)
+        print('nop.__getattr__',name)
         pass
         
 class set(nop):
@@ -70,4 +99,7 @@ class wrap(nop):
          super().__init__(*args, **kwargs)
          pass 
          
+#@decorator(like = "geeksforgeeks")
+#def my_func():
+    #print("Inside actual function")
          
